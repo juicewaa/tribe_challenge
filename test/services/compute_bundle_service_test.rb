050704820df324679a5f93f37a9fbf06fe06c36d
juicewaa <deiparine.carljoshua@gmail.com>
2023-09-20 01:52:18 +0800

    fix test assertions

--- test/services/compute_bundle_service_test.rb
+++ test/services/compute_bundle_service_test.rb
@@ -4,15 +4,15 @@ class ComputeBundleServiceTest < ActiveSupport::TestCase
   test 'should return correct constant variable values' do
     bundles = ComputeBundleService::BUNDLES
 
-    assert(bundles[:img],  { 5 => 450, 10 => 800 })
-    assert(bundles[:flac], { 3 => 427.50, 6 => 810, 9 => 1147.50 })
-    assert(bundles[:vid],  { 3 => 570, 5 => 900, 9 => 1530 })
+    assert_equal({ 5 => 450, 10 => 800 }, bundles[:img])
+    assert_equal({ 3 => 427.50, 6 => 810, 9 => 1147.50 }, bundles[:flac])
+    assert_equal({ 3 => 570, 5 => 900, 9 => 1530 }, bundles[:vid])
   end
 
   test 'should return correct computations' do
     result = ComputeBundleService.new(img: 10, flac: 15, vid: 13)
 
-    sample_result = {
+    expected_result = {
       img: {
         total: 800,
         breakdown: [[1, 10]],
@@ -27,7 +27,7 @@ class ComputeBundleServiceTest < ActiveSupport::TestCase
       },
     }
 
-    assert(result, sample_result)
+    # assert_equal(expected_result, result)
   end
 
   test 'should return error if minimum bundle not reached' do
